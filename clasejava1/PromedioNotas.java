3f3c7c39ae183bccbf6a1be4a8e380f1d193f06a
Diego Agüero <Diego_c-walker17@hotmail.com>
2016-06-23 15:47:21 -0300

    Add files via upload

---
+++ clasejava1/PromedioNotas.java
@@ -0,0 +1,12 @@
+package clasejava1;
+
+
+public class PromedioNotas {
+    public static void main(String[] args) {
+        double n1=5.0;
+        double n2=4.5;
+        double n3=4.9;
+        double promedio = (n1+n2+n3)/3;
+        System.out.println("El promedio de 3 notas es : "+promedio);
+    }
+}
